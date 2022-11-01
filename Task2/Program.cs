869e86a1d172d1b7ba752614a47e0655b0c1caf0
Tanya <vershinina572@gmail.com>
2022-11-01 17:46:58 +0100

    Task1

---
+++ Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+if (a > b){
+    Console.Write("a = " + a + "; b = " + b + " -> max = " + a + ", min = " + b);
+} else {
+    Console.Write("a = " + a + "; b = " + b + " -> max = " + b + ", min = " + a);
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+if (a > b && a > c){
+    Console.Write( a + ", " + b + ", " + c + " -> " + a);
+} else if(b > a && b > c) {
+    Console.Write(  a + ", " + b + ", " + c + " -> " + b);
+} else {
+    Console.Write( a + ", " + b + ", " + c + " -> " + c);
+}
