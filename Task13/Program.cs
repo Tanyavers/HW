b36607b58458b224321f45738320585eb061e95b
Tanya <vershinina572@gmail.com>
2022-11-03 17:07:56 +0100

    finish task

---
+++ Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите число: ");
+string a = Console.ReadLine();
+char[] b = a.ToCharArray();
+if (b.Length>2){
+    string c = a+" -> "+b[2];
+    Console.Write(c);
+}
+else{
+    string c = a+" -> "+"Третьей цифры нет";
+    Console.Write(c);
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Введите цифру: ");
+string a = Console.ReadLine();
+int b = int.Parse(a);
+if (b==6 || b==7){
+    string c = a+" -> Да";
+    Console.Write(c);
+}
+else{
+    string c = a+" -> Нет";
+    Console.Write(c);
+}
