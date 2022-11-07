9a3ea6528565d98db266e647bfb3b7ed5af6d54b
Tanya <vershinina572@gmail.com>
2022-11-07 16:34:55 +0100

    finish task

---
+++ Task19/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Write("Введите пятизначное число: ");
+string a = Console.ReadLine();
+string b = new string(a.Reverse().ToArray());
+string c = a + " -> ";
+if (a == b)
+    Console.Write(c + "да");
+else
+    Console.Write(c + "нет");
---
+++ Task23/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int[] b = new int[a];
+for (int i = 1; i <= a; i++){
+    int c = i*i*i;
+    b[i - 1]= c;
+}
+Console.Write(a + " -> " + string.Join(", ", b));
