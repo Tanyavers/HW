6fcc75bb2273a467baa61d4dbb376fe3b91b6bc7
Tanya <vershinina572@gmail.com>
2022-11-11 15:27:50 +0100

    finish task

---
+++ Task41/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Write("Введите числа: ");
+string a = Console.ReadLine();
+string[] b = a.Split(",");
+int c = 0;
+int d;
+if(a==string.Join(" ", b)){
+    b=a.Split(" ");
+}
+for(int i = 0; i < b.Length; i++){
+    if(int.TryParse(b[i],out d) && int.Parse(b[i])>0){
+        c++;
+    }
+}
+Console.Write("["+string.Join(", ", b)+"] -> "+c);
---
+++ Task43/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+Console.Write("Введите b1: ");
+float b1 = float.Parse(Console.ReadLine());
+Console.Write("Введите k1: ");
+float k1 = float.Parse(Console.ReadLine());
+Console.Write("Введите b2: ");
+float b2 = float.Parse(Console.ReadLine());
+Console.Write("Введите k2: ");
+float k2 = float.Parse(Console.ReadLine());
+
+float x=(b2 - b1)/(k1 - k2);
+float y=k1 * (b2 - b1)/(k1 - k2) + b1;
+
+Console.Write("b1 = "+b1+", k1 = "+k1+", b2 = "+b2+", k2 = "+k2+" -> ("+x+";"+y+")");
